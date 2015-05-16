8a121d4a6f62cfa1576722cb0e18502ef6493304
Byron Adams <byron@webtools.co.nz>
2015-05-16 20:27:59 +1200

    disabled task, adding test, test

---
+++ tests/unit/Test.php
@@ -0,0 +1,20 @@
+<?php
+namespace webtoolsnz\scheduler\tests;
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+        $this->assertEquals(true, true);
+    }
+
+}
